46913933a6f911cae3c0174cc53029a975a24cf3
unknown <albemako@yahoo.com>
2017-10-27 14:35:30 -0600

    pull

--- Lab3_ProgramacionII/src/lab3_programacionii/Lab3_ProgramacionII.java
+++ Lab3_ProgramacionII/src/lab3_programacionii/Lab3_ProgramacionII.java
@@ -5,6 +5,10 @@
  */
 package lab3_programacionii;
 //puse un comentario
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author RALC
@@ -15,7 +19,39 @@ public class Lab3_ProgramacionII {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(" hola");
+        ArrayList <Personas>personas=new ArrayList();
+        
+        //login
+        int resp=1, resp2=1;
+                do{
+                        resp2=1;
+                    do{
+                        resp=1;
+                        String user="", pass="";
+                        user=JOptionPane.showInputDialog("Username");
+                        pass=JOptionPane.showInputDialog("Password");
+                        for (int i = 0; i < personas.size(); i++) {
+                            if(personas.get(i).getUsername().contains(user) && personas.get(i).getPassword().contains(pass)){
+                                if(personas.getSocio){
+                                    System.out.println("hola socio");
+                                }else{
+                                    if(personas.getEmpleado_cliente){
+                                        System.out.println("hola cliente");
+                                    }else{
+                                        System.out.println("hola empleado");
+                                    }
+                                }
+                            }
+                            else{
+                                resp=3;
+                            }
+                        }
+                    }while(resp==0);
+                    if(resp==3){
+                       JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrecta");
+                       resp2=0;
+                    }
+                }while(resp2==0);
     }
     
 }
